ed9f3d79cc5d9b671b1491db2c7ea03d901b9f4f
guialves96 <guilherme.alves.lourenco@gmail.com>
2024-11-09 15:16:58 -0300

    feat: add login page

--- app/transaction/page.tsx
+++ app/transaction/page.tsx
@@ -1,5 +1,5 @@
-const Transaction = () => {
-    return <h1> Transaction Page</h1>;
-}
+const TransactionPage = () => {
+  return <h1> Transaction Page</h1>;
+};
 
-export default Transaction;
+export default TransactionPage;
